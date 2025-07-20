89ca7ab01888c84f21fb65f33bf356371d4f6879
samuelramdial <samuelramdial0327@gmail.com>
2025-07-20 03:46:24 -0400

    Changing button text

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -15,7 +15,7 @@ export const AboutSection = () => {
                     <div className = "flex flex-col sm: flex-row gap-4 pt-4 justify-center">
                         <a href = "#contact" className="cosmic-button">Get In Touch</a>
                         <a href = "https://www.linkedin.com/in/samuel-ramdial/overlay/1752997350507/single-media-viewer/?profileId=ACoAADLranIBfCsRHXtYC_WiMf8lxKGBqHkP2os" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
-                            Download CV
+                            View Resume
                         </a>
                     </div>
                 </div>
